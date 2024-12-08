4fe0c0b1e1b892aa019fd7ee7619162f0e27344a
riecodes <eirmonpaculan11@gmail.com>
2024-12-08 13:26:15 +0800

    a lot happened

--- chefdonburi/src/Database/Database.java
+++ chefdonburi/src/Database/Database.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 public class Database {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/chefdonburiinventory";
+    private static final String URL = "jdbc:mysql://localhost:3308/chefdonburiinventory";
     private static final String USER = "root";
-    private static final String PASSWORD = "";
+    private static final String PASSWORD = ""; // Update password if needed
 
     public Connection getConnection() throws SQLException {
     Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
